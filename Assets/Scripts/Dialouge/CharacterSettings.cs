e8c0ec5386671e93dcc5c7ae5f370b0ae7ca128a
MrAporia <114781092+BrettPirro@users.noreply.github.com>
2026-01-31 15:55:40 -0500

    character update

--- Assets/Scripts/Dialouge/CharacterSettings.cs
+++ Assets/Scripts/Dialouge/CharacterSettings.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class CharacterSettings : ScriptableObject
 {
     public List<CharacterDialougeOBJ> TiedTree;
-
+    public int preSyncNumber;
+    public string preSyncName;
+    public bool sirNameCanEndWithLetterPastP;
     public List<Sprite> head;
     public List<Sprite> neck;
     public List<Sprite> body;
